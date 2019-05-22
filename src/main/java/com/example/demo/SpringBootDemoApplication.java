0da2541baac1fc6bcb296af23acedacbcbd91aef
匡鹏 <kuangpeng@cmhi.chinamobile.com>
2019-05-22 19:34:31 +0800

    change

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -9,4 +9,6 @@ public class SpringBootDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
+
+	// this is the end of the main method
 }
